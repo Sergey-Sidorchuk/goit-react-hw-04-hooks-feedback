59b50e1653e159a948aaf651ba9e9ce2b381a4e5
SergeySidorchuk <serg_ss@ukr.net>
2021-08-03 02:05:59 +0300

    Added Feedback.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FeedbackOptions.module.css';
 
 
-
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (<>
     {options.map(option => 
-        <button type='button'
+        <button
+            type='button'
             key={option}
             className={styles.button}
             onClick={() => onLeaveFeedback(option)}>
@@ -14,9 +14,11 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => (<>
         </button>
     )} </>   
 );
+
+
 FeedbackOptions.propTypes = {
-    options: PropTypes.array,
-     onLeaveFeedback: PropTypes.func.isRequired,
+    options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback: PropTypes.func,
 
 }
 
