20e6719babea904722b5657aa292b3e9d178e179
SergeySidorchuk <serg_ss@ukr.net>
2021-08-02 17:56:11 +0300

    added App/Hooks

--- src/App.js
+++ src/App.js
@@ -1,73 +1,126 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import { useState } from "react";
 import './App.css';
 
 
-import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
-import Section from './components/Section/Section';
-import Statistics from "./components/Statistics/Statistics";
-import Notification from './components/Notification/Notification';
+import FeedbackOptions from "./components/FeedbackOptions";
+import Section from './components/FeedbackOptions';
+import Statistics from "./components/Statistics";
+import Notification from './components/Notification';
 
 
-
-
-class App extends Component {
-    static defaultProps = {
-        totalFeedbacks: 0,
-        percentageOfGoodFeedback:0,
-    }
-    static propTypes = {
-        totalFeedbacks: PropTypes.number,
-        percentageOfGoodFeedback: PropTypes.number,
-        good: PropTypes.number,
-        bad: PropTypes.number,
-        neutral: PropTypes.number,
-        options: PropTypes.arrayOf(PropTypes.number),
-    };
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    };
-
-    onLeaveFeedback = (option) => {
-        this.setState(prevState => {
-            return ({
-                [option]: prevState[option] +1
-            })
-        })       
-    }
-
-    countTotalFeedback() {
-        const { good, neutral, bad } = this.state;
+export default function App() {
+const [good, setGood] = useState(0);
+const [neutral, setNeutral] = useState(0);
+const [bad, setBad] = useState(0);
+    
+    
+    const countTotalFeedback = () => {
         const totalFeedbacks = good + neutral + bad;
         return totalFeedbacks;
-    }
-    countPositiveFeedbackPercentage() {
-        const { good, neutral, bad } = this.state;
+    };
+    const countPositiveFeedbackPercentage = () => {
         const percentageOfGoodFeedback = ((good / (good + neutral + bad)) * 100).toFixed(2);
         return percentageOfGoodFeedback;
-    }
-
-    render(totalFeedbacks) {
-    const { good, neutral, bad } = this.state;
+    };
+    
+    const onLeaveFeedback = (event) => {
+        switch (event.target.name) {
+            case "good":
+                setGood((prevState) => prevState + 1);
+                break;
+            case "neutral":
+                    setNeutral((prevState) => prevState + 1);
+                    break;
+            case "bad":
+                        setBad((prevState) => prevState + 1);
+                        break;
+                    
+                    default:
+                            return;
+        }
+    };
+    
     return (
-      <div className="App">
-          <h1>Please leave feedback</h1>  
+        <div className="App">
+            <h1>Please leave feedback</h1>
 
         <Section title="">
-          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={this.onLeaveFeedback}></FeedbackOptions>
+                <FeedbackOptions options=
+                    {
+                        ['good', 'neutral', 'bad']
+                    }
+                    onLeaveFeedback={onLeaveFeedback}>
+                    
+                    </FeedbackOptions>
         </Section>
-            {(totalFeedbacks !== 0)
+            {
+                countTotalFeedback() !== 0
                 ? <Section title="Statistics">
-                    <Statistics  good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()} />
+                    <Statistics  good={good} neutral={neutral} bad={bad} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()} />
                 </Section>
                 : < Notification message="No feedback given" />
             }
-      </div>
+    </div>
     );
-  }  
 }
+// class App extends Component {
+//     static defaultProps = {
+//         totalFeedbacks: 0,
+//         percentageOfGoodFeedback:0,
+//     }
+//     static propTypes = {
+//         totalFeedbacks: PropTypes.number,
+//         percentageOfGoodFeedback: PropTypes.number,
+//         good: PropTypes.number,
+//         bad: PropTypes.number,
+//         neutral: PropTypes.number,
+//         options: PropTypes.arrayOf(PropTypes.number),
+//     };
+//     state = {
+//         good: 0,
+//         neutral: 0,
+//         bad: 0,
+//     };
+
+//     onLeaveFeedback = (option) => {
+//         this.setState(prevState => {
+//             return ({
+//                 [option]: prevState[option] +1
+//             })
+//         })       
+//     }
+
+//     countTotalFeedback() {
+//         const { good, neutral, bad } = this.state;
+//         const totalFeedbacks = good + neutral + bad;
+//         return totalFeedbacks;
+//     }
+//     countPositiveFeedbackPercentage() {
+//         const { good, neutral, bad } = this.state;
+//         const percentageOfGoodFeedback = ((good / (good + neutral + bad)) * 100).toFixed(2);
+//         return percentageOfGoodFeedback;
+//     }
+
+//     render(totalFeedbacks) {
+//     const { good, neutral, bad } = this.state;
+//     return (
+//       <div className="App">
+//           <h1>Please leave feedback</h1>  
+
+//         <Section title="">
+//           <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={this.onLeaveFeedback}></FeedbackOptions>
+//         </Section>
+//             {(totalFeedbacks !== 0)
+//                 ? <Section title="Statistics">
+//                     <Statistics  good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()} />
+//                 </Section>
+//                 : < Notification message="No feedback given" />
+//             }
+//       </div>
+//     );
+//   }  
+// }
 
 
-export default App;
+// export default App;
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -6,7 +6,7 @@ import styles from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (<>
     {options.map(option => 
-        <button type='button' key={option} className={styles.[option]} onClick={()=>onLeaveFeedback(option)}>{option}</button>
+        <button type='button' key={option} className={styles.button} onClick={()=>onLeaveFeedback(option)}>{option}</button>
     )} </>   
 );
 FeedbackOptions.propTypes = {
